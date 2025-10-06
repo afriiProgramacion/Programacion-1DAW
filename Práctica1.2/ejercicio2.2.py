c5f837c8431531bd1f29c8483a67c6d6e1a3cc25
agalindos <agalindos@iesrafaelalberti.es>
2025-10-06 21:10:11 +0200

    ejercicio 2.2 terminado y funcionando

---
+++ Práctica1.2/ejercicio2.2.py
@@ -0,0 +1,4 @@
+Horas_Trabajo = input ("¿Cuántas horas has trabajado?")
+Coste_Hora = input ("¿Cuánto cuesta la hora?")
+Importe_Total = Horas_Trabajo * Coste_Hora
+print ("El importe total del servicio es:", Importe_Total)

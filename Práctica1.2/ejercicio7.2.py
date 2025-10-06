e1da3c6b77784de88ee09d231a3e4b1d28d4293b
agalindos <agalindos@iesrafaelalberti.es>
2025-10-06 21:21:30 +0200

    ej7 modified

--- Práctica1.2/ejercicio7.2.py
+++ Práctica1.2/ejercicio7.2.py
@@ -1,5 +1,5 @@
 num1 = int(input("Introduce un número: "))
 num2 = int(input("Introduce otro número: "))
-num3 = int(input("Introduce el último número"))
+num3 = int(input("Introduce el último número: "))
 suma = num1 + num2 + num3
 print ("La suma de estos números es:", suma)

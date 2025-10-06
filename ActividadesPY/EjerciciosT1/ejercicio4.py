c29eb9b8656b5f736fce935e710b37bc58f4723b
agalindos <agalindos@iesrafaelalberti.es>
2025-10-06 20:00:25 +0200

    ejercicio4.py editado, pero funcionando

--- ActividadesPY/EjerciciosT1/ejercicio4.py
+++ ActividadesPY/EjerciciosT1/ejercicio4.py
@@ -1,3 +1,3 @@
-Grados = int(input("Introduce una temperatura en grados celsius: "))
-Farenheit = (Grados * 1,8) + 32
+Celsius = float(input("Introduce una temperatura en grados celsius: "))
+Farenheit = (Celsius * 9/5) + 32
 print("La temperatura en grados Farenheit es: ", Farenheit)

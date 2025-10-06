a66980fe1de771ef32a47a428f685bdf0d29ba52
agalindos <agalindos@iesrafaelalberti.es>
2025-10-06 19:38:47 +0200

    dos ramas juntas

---
+++ ActividadesPY/EjerciciosT1/ejercicio3.py
@@ -0,0 +1,10 @@
+ancho = 17
+alto = 12.0
+divnorm = ancho / 2
+divent = ancho // 2
+divalt = alto / 3
+sum = 1 + 2  + 3 * 5
+print ("La división normal es igual a: ", divnorm)
+print ("La división entera es igual a: ", divent)
+print ("La división del alto es igual a: ", divalt)
+print ("La suma es: ", sum)

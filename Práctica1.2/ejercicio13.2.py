551c2ef3cd64a994cec83dcd0ab12933a2720bcb
agalindos <agalindos@iesrafaelalberti.es>
2025-10-09 19:50:31 +0200

    ejercicio13

--- Práctica1.2/ejercicio13.2.py
+++ Práctica1.2/ejercicio13.2.py
@@ -1,1 +1,5 @@
-num1 = int(input())
+num1 = int(input("Introduce un número entero: "))
+num2 = int(input("Introduce otro nmero entero: "))
+r = num1 % num2
+c = num1 // num2
+print ("La división de", num1, "entre", num2,  "da como cociente", c, "y un resto", r)

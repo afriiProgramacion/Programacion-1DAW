c5afc0adeca5c0305d834d0afbeefd9a583154b5
agalindos <agalindos@iesrafaelalberti.es>
2025-10-16 18:59:51 +0200

    cambios

---
+++ Práctica1.2/ejercicio15.2.py
@@ -0,0 +1,8 @@
+cantidad_depositada = int(input("¿Qué cantidad de dinero has depositado en la cuenta?"))
+porcentaje_interes = 0.4
+
+año1 = cantidad_depositada * (1 + 0.4)
+año2 = año1 * (1 + 0.4)
+año3 = año2 * (1 + 0.4)
+
+

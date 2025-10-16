b4aa8c25c41b21f6808d324a701628a05b6a15e3
agalindos <agalindos@iesrafaelalberti.es>
2025-10-16 19:03:15 +0200

    nuevos cambios

--- Práctica1.2/ejercicio14.2.py
+++ Práctica1.2/ejercicio14.2.py
@@ -7,12 +7,5 @@ npayasos = int(input("¿Cuántos payasos has vendido en el último pedido?: "))
 pmuñecas = muñecas * nmuñecas
 ppayasos = payasos * npayasos
 
-if (npayasos <= 0 and nmuñecas >=0):   
-    print ("El peso del paquete es:", pmuñecas, "g")
-
-elif (npayasos >=0 and nmuñecas <=0):
-    print ("El peso del paquete es:", ppayasos, "g")
-
-else: 
-    pfinal = ppayasos + pmuñecas
-    print("El peso total del paquete incluyendo", nmuñecas, "muñecas y", npayasos, "payasos es:", pfinal, "g")
+total = pmuñecas + ppayasos
+print ("El paquete pesa:", total, "g")
--- Práctica1.2/ejercicio15.2.py
+++ Práctica1.2/ejercicio15.2.py
@@ -5,4 +5,6 @@ año1 = cantidad_depositada * (1 + 0.4)
 año2 = año1 * (1 + 0.4)
 año3 = año2 * (1 + 0.4)
 
-
+print("El primer año la cantidad de ahorros fue:", año1)
+print("El segundo año la cantidad fue de:", año2)
+print("Y el tercero, la cantidad fue de:", año3)
